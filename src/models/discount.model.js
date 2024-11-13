9c894c4d5bf3e65a119a92d95ab09b2bfadddf89
vmdt <vmdt03@gmail.com>
2024-11-13 09:19:44 +0700

    [fix]: fix total order in get discount amount

--- src/models/discount.model.js
+++ src/models/discount.model.js
@@ -1,20 +1,28 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Discount = new mongoose.Schema({
-    name: String,
-    code: { type: String, required: true },
-    value: { type: Number, required: true },
-    type: { type: String, default: 'fixed_amount', enum: ['fixed_amount', 'percentage'] },
-    startDate: Date,
-    endDate: Date,
-    maxUses: { type: Number, default: 50 },
-    usedCount: { type: Number, default: 0 },
-    minOrder: { type: Number, default: 0 },
-    isActive: { type: Boolean, default: true },
-    appliesTo: { type: String, enum: ['total_order', 'specific'], required: true },
-    tours: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Tour' }]
+	name: String,
+	code: { type: String, required: true },
+	value: { type: Number, required: true },
+	type: {
+		type: String,
+		default: "fixed_amount",
+		enum: ["fixed_amount", "percentage"],
+	},
+	startDate: Date,
+	endDate: Date,
+	maxUses: { type: Number, default: 50 },
+	usedCount: { type: Number, default: 0 },
+	minOrder: { type: Number, default: 0 },
+	isActive: { type: Boolean, default: true },
+	appliesTo: {
+		type: String,
+		enum: ["total_order", "specific"],
+		required: true,
+	},
+	tours: [{ type: mongoose.Schema.Types.ObjectId, ref: "Tour" }],
 });
 
-Discount.index({ code: 'text' });
+Discount.index({ code: "text" });
 
-module.exports = mongoose.model('Discount', Discount);
+module.exports = mongoose.model("Discount", Discount);
--- src/services/discount.service.js
+++ src/services/discount.service.js
@@ -1,203 +1,249 @@
-const _ = require('lodash');
-const { Types } = require('mongoose');
-const DiscountModel = require('../models/discount.model');
-const TourModel = require('../models/tour.model');
-const { getOne, createOne, getAll, getMany, updateOne } = require('../repositories/factory.repo');
-const { BadRequestError, NotFoundError } = require('../utils/error.response');
+const _ = require("lodash");
+const { Types } = require("mongoose");
+const DiscountModel = require("../models/discount.model");
+const TourModel = require("../models/tour.model");
+const {
+	getOne,
+	createOne,
+	getAll,
+	getMany,
+	updateOne,
+} = require("../repositories/factory.repo");
+const { BadRequestError, NotFoundError } = require("../utils/error.response");
 
 class DiscountService {
-    static createDiscount = async (payload) => {
-        const { 
-            name, code, value, type,
-            startDate, endDate, minOrder,
-            isActive, tours, appliesTo
-        } = payload;
-        const discountExisting = await DiscountModel.findOne({ code }).lean();
+	static createDiscount = async (payload) => {
+		const {
+			name,
+			code,
+			value,
+			type,
+			startDate,
+			endDate,
+			minOrder,
+			isActive,
+			tours,
+			appliesTo,
+		} = payload;
+		const discountExisting = await DiscountModel.findOne({ code }).lean();
 
-        if (discountExisting)
-            throw new BadRequestError('Discount already exists');
+		if (discountExisting) throw new BadRequestError("Discount already exists");
 
-        const discount = await createOne(DiscountModel, {
-            name, code, value, type,
-            startDate: new Date(startDate),
-            endDate: new Date(endDate),
-            minOrder, isActive, appliesTo,
-            tours: appliesTo === 'total_order' ? [] : tours
-        });
+		const discount = await createOne(DiscountModel, {
+			name,
+			code,
+			value,
+			type,
+			startDate: new Date(startDate),
+			endDate: new Date(endDate),
+			minOrder,
+			isActive,
+			appliesTo,
+			tours: appliesTo === "total_order" ? [] : tours,
+		});
 
-        return { discount }
-    }
+		return { discount };
+	};
 
-    static searchDiscount = async (code) => {
-        const foundDiscounts = await DiscountModel.find({
-            isActive: true,
-            code: { $regex:`^${code}`, $options: 'iu' }
-        }).lean();
+	static searchDiscount = async (code) => {
+		const foundDiscounts = await DiscountModel.find({
+			isActive: true,
+			code: { $regex: `^${code}`, $options: "iu" },
+		}).lean();
 
-        return {
-            result: foundDiscounts.length,
-            discounts: foundDiscounts
-        }
-    }
+		return {
+			result: foundDiscounts.length,
+			discounts: foundDiscounts,
+		};
+	};
 
-    static getAllDiscounts = async (query) => {
-        const {total, docs: discounts} = await getAll(DiscountModel, query);
-        return {
-            total,
-            result: discounts.length,
-            discounts
-        }
-    }
+	static getAllDiscounts = async (query) => {
+		const { total, docs: discounts } = await getAll(DiscountModel, query);
+		return {
+			total,
+			result: discounts.length,
+			discounts,
+		};
+	};
 
-    static getActiveDiscounts = async (query) => {
-        const {total, docs: discounts} = await getMany(DiscountModel, {
-            isActive: true
-        }, query);
+	static getActiveDiscounts = async (query) => {
+		const { total, docs: discounts } = await getMany(
+			DiscountModel,
+			{
+				isActive: true,
+			},
+			query,
+		);
 
-        return {
-            total,
-            result: discounts.length,
-            discounts
-        }
-    }
+		return {
+			total,
+			result: discounts.length,
+			discounts,
+		};
+	};
 
-    static getInactiveDiscounts = async (query) => {
-        const {total, docs: discounts} = await getMany(DiscountModel, {
-            isActive: true
-        }, query);
+	static getInactiveDiscounts = async (query) => {
+		const { total, docs: discounts } = await getMany(
+			DiscountModel,
+			{
+				isActive: true,
+			},
+			query,
+		);
 
-        return {
-            total,
-            result: discounts.length,
-            discounts
-        }
-    }
+		return {
+			total,
+			result: discounts.length,
+			discounts,
+		};
+	};
 
-    static activateDiscount = async (discountId) => {
-        const discount = await updateOne(DiscountModel, {
-            _id: new Types.ObjectId(discountId)
-        }, { isActive: true });
-        if (!discount)
-            throw new NotFoundError('Not found discount');
-        return {
-            discount: discount.toObject()
-        }
-    }
+	static activateDiscount = async (discountId) => {
+		const discount = await updateOne(
+			DiscountModel,
+			{
+				_id: new Types.ObjectId(discountId),
+			},
+			{ isActive: true },
+		);
+		if (!discount) throw new NotFoundError("Not found discount");
+		return {
+			discount: discount.toObject(),
+		};
+	};
 
-    static deactivateDiscount = async (discountId) => {
-        const discount = await updateOne(DiscountModel, {
-            _id: new Types.ObjectId(discountId)
-        }, { isActive: false });
-        if (!discount)
-            throw new NotFoundError('Not found discount');
-        return {
-            discount: discount.toObject()
-        }
-    }
+	static deactivateDiscount = async (discountId) => {
+		const discount = await updateOne(
+			DiscountModel,
+			{
+				_id: new Types.ObjectId(discountId),
+			},
+			{ isActive: false },
+		);
+		if (!discount) throw new NotFoundError("Not found discount");
+		return {
+			discount: discount.toObject(),
+		};
+	};
 
-    static getDiscountById = async (discountId) => {
-        const discount = await getOne(DiscountModel, {
-            _id: new Types.ObjectId(discountId)
-        });
-        if (!discount)
-            throw new NotFoundError('Not found discount');
-        if (!discount.isActive)
-            throw new BadRequestError('Discount has been deactivated');
+	static getDiscountById = async (discountId) => {
+		const discount = await getOne(DiscountModel, {
+			_id: new Types.ObjectId(discountId),
+		});
+		if (!discount) throw new NotFoundError("Not found discount");
+		if (!discount.isActive)
+			throw new BadRequestError("Discount has been deactivated");
 
-        return discount;
-    }
+		return discount;
+	};
 
-    static updateDiscount = async (discountId, payload) => {
-        const discountExisting = await DiscountModel.findById(
-            new Types.ObjectId(discountId)
-        ).lean();
-        if (!discountExisting)
-            throw new NotFoundError('Not found discount');
-        if (!discountExisting.isActive)
-            throw new BadRequestError('Discount has been deactivated');
+	static updateDiscount = async (discountId, payload) => {
+		const discountExisting = await DiscountModel.findById(
+			new Types.ObjectId(discountId),
+		).lean();
+		if (!discountExisting) throw new NotFoundError("Not found discount");
+		if (!discountExisting.isActive)
+			throw new BadRequestError("Discount has been deactivated");
 
-        const discount = await updateOne(DiscountModel, {
-            _id: new Types.ObjectId(discountId)
-        }, payload);
-        return {
-            discount: discount.toObject()
-        }
-    }
+		const discount = await updateOne(
+			DiscountModel,
+			{
+				_id: new Types.ObjectId(discountId),
+			},
+			payload,
+		);
+		return {
+			discount: discount.toObject(),
+		};
+	};
 
-    static getToursByDiscountCode = async (code, query) => {
-        const discount = await DiscountModel.findOne({ code }).lean();
-        if (!discount)
-            throw new NotFoundError('Not found discount');
-        if (!discount.isActive)
-            throw new BadRequestError('Discount has been deactivated');
+	static getToursByDiscountCode = async (code, query) => {
+		const discount = await DiscountModel.findOne({ code }).lean();
+		if (!discount) throw new NotFoundError("Not found discount");
+		if (!discount.isActive)
+			throw new BadRequestError("Discount has been deactivated");
 
-        const {total, docs: tours} = await getMany(TourModel, {
-            _id: { $in: discount.tours }
-        }, query, true);
+		const { total, docs: tours } = await getMany(
+			TourModel,
+			{
+				_id: { $in: discount.tours },
+			},
+			query,
+			true,
+		);
 
-        return { 
-            total,
-            result: tours.length,
-            tours    
-        };
-    }
+		return {
+			total,
+			result: tours.length,
+			tours,
+		};
+	};
 
-    static getDiscountAmount = async ({ code, tours }) => {
-        const discountExisting = await DiscountModel.findOne({ code }).lean();
-        if (!discountExisting)
-            throw new NotFoundError('Not found discount');
-        
-        const {
-            isActive, startDate, endDate,
-            maxUses, usedCount, minOrder,
-            type, value, appliesTo
-        } = discountExisting;
+	static getDiscountAmount = async ({ code, tours }) => {
+		const discountExisting = await DiscountModel.findOne({ code }).lean();
+		if (!discountExisting) throw new NotFoundError("Not found discount");
 
-        if (!isActive)
-            throw new BadRequestError('Discount has been deactivated');
+		const {
+			isActive,
+			startDate,
+			endDate,
+			maxUses,
+			usedCount,
+			minOrder,
+			type,
+			value,
+			appliesTo,
+		} = discountExisting;
 
-        const currentDate = new Date().setHours(0, 0, 0, 0);
-        if (endDate < currentDate || currentDate < startDate)
-            throw new BadRequestError('Invalid discount');
+		if (!isActive) throw new BadRequestError("Discount has been deactivated");
 
-        if (usedCount >= maxUses)
-            throw new BadRequestError('Discount ran out');
+		const currentDate = new Date().setHours(0, 0, 0, 0);
+		if (endDate < currentDate || currentDate < startDate)
+			throw new BadRequestError("Invalid discount");
 
-        const totalOrder = tours.reduce((acc, tour) => {
-            return acc + tour.totalPrice;
-        }, 0);
-        
-        if (totalOrder < minOrder)
-            throw new BadRequestError(`Discount require min order of ${minOrder}`);
-        let amount = 0, discountTours = [], discountPrice;
-        if (appliesTo === 'specific') {
-            tours.forEach((tour) => {
-                const match = discountExisting.tours.some(el => {
-                    return el.toString() === tour.tourId.toString();
-                });
-                if (match) {
-                    discountPrice = type === 'fixed_amount' ? value : tour.totalPrice * (value/100);
-                    amount += discountPrice;
-                    tour.discountPrice = discountPrice;
-                    discountTours.push({ tour });
-                }
-            })
-        } else {
-            discountPrice = type === 'fixed_amount' ? value : tour.totalPrice * (value/100);
-            amount += discountPrice;
-            tour.discountPrice = discountPrice;
-            discountTours.push({ tour });
-        }
+		if (usedCount >= maxUses) throw new BadRequestError("Discount ran out");
 
-        return {
-            itemPrices: discountTours,
-            checkoutOrder: {
-                totalOrder,
-                discount: amount,
-                totalPrice: totalOrder - amount
-            }
-        };
-    }
+		const totalOrder = tours.reduce((acc, tour) => {
+			return acc + tour.totalPrice;
+		}, 0);
+
+		if (totalOrder < minOrder)
+			throw new BadRequestError(`Discount require min order of ${minOrder}`);
+		let amount = 0,
+			discountTours = [],
+			discountPrice;
+		if (appliesTo === "specific") {
+			tours.forEach((tour) => {
+				const match = discountExisting.tours.some((el) => {
+					return el.toString() === tour.tourId.toString();
+				});
+				if (match) {
+					discountPrice =
+						type === "fixed_amount" ? value : tour.totalPrice * (value / 100);
+					amount += discountPrice;
+					tour.discountPrice = discountPrice;
+					discountTours.push({ tour });
+				}
+			});
+		} else {
+			tours.forEach((tour) => {
+				discountPrice =
+					type === "fixed_amount" ? value : tour.totalPrice * (value / 100);
+				amount += discountPrice;
+				tour.discountPrice = discountPrice;
+				discountTours.push({ tour });
+			});
+		}
+
+		return {
+			itemPrices: discountTours,
+			checkoutOrder: {
+				totalOrder,
+				discount: amount,
+				totalPrice: totalOrder - amount,
+			},
+		};
+	};
 }
 
 module.exports = DiscountService;
