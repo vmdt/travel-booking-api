5653a504e7ca353e3fa02b48e8ddb1d0580e9ddb
vmdt <vmdt03@gmail.com>
2024-12-02 22:53:02 +0700

    [fix]: disable sort by createAt

--- src/helpers/api.features.js
+++ src/helpers/api.features.js
@@ -1,57 +1,55 @@
 class APIFeatures {
-    constructor(query, queryObj) {
-        this.query = query;
-        this.queryObj = queryObj;
-    }
+	constructor(query, queryObj) {
+		this.query = query;
+		this.queryObj = queryObj;
+	}
 
-    filter() {
-        const queryObj = {...this.queryObj};
-        const excludedFields = ['page', 'sort', 'limit', 'fields'];
-        excludedFields.forEach(el => delete queryObj[el]);
+	filter() {
+		const queryObj = { ...this.queryObj };
+		const excludedFields = ["page", "sort", "limit", "fields"];
+		excludedFields.forEach((el) => delete queryObj[el]);
 
-        let queryStr = JSON.stringify(queryObj);
-        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+		let queryStr = JSON.stringify(queryObj);
+		queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
-        this.query.find(JSON.parse(queryStr));
-        this.totalQuery = this.query.clone();
-        return this;
-    }
+		this.query.find(JSON.parse(queryStr));
+		this.totalQuery = this.query.clone();
+		return this;
+	}
 
-    sort() {
-        if (this.queryObj.sort) {
-            const sortBy = this.queryObj.sort.split(',').join(' ');
-            this.query = this.query.sort(sortBy);
-        } else {
-            this.query = this.query.sort('-createdAt');
-        }
+	sort() {
+		if (this.queryObj.sort) {
+			const sortBy = this.queryObj.sort.split(",").join(" ");
+			this.query = this.query.sort(sortBy);
+		}
 
-        return this;
-    }
+		return this;
+	}
 
-    limitFields() {
-        if (this.queryObj.fields) {
-            const fields = this.queryObj.fields.split(',').join(' ');
-            this.query = this.query.select(fields);
-        } else {
-            this.query = this.query.select('-__v');
-        }
+	limitFields() {
+		if (this.queryObj.fields) {
+			const fields = this.queryObj.fields.split(",").join(" ");
+			this.query = this.query.select(fields);
+		} else {
+			this.query = this.query.select("-__v");
+		}
 
-        return this;
-    }
+		return this;
+	}
 
-    paginate() {
-        const page = this.queryObj.page * 1 || 1;
-        const limit = this.queryObj.limit * 1 || 100;
-        const skip = (page - 1) * limit;
+	paginate() {
+		const page = this.queryObj.page * 1 || 1;
+		const limit = this.queryObj.limit * 1 || 100;
+		const skip = (page - 1) * limit;
 
-        this.query = this.query.skip(skip).limit(limit);
+		this.query = this.query.limit(limit).skip(skip);
 
-        return this;
-    }
+		return this;
+	}
 
-    async getTotalDocs() {
-        return await this.totalQuery.countDocuments();
-    }
+	async getTotalDocs() {
+		return await this.totalQuery.countDocuments();
+	}
 }
 
 module.exports = APIFeatures;
