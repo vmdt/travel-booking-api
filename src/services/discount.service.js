b14d87f0e011146779b44f46a56b616e39a4a0e6
vmdt <vmdt03@gmail.com>
2025-05-24 16:22:29 +0700

    [fix]: update timezone (testing): build:travelife-be

--- src/services/discount.service.js
+++ src/services/discount.service.js
@@ -207,7 +207,7 @@ class DiscountService {
 		);
 
 		if (discountExisting.taskQueueId) {
-			const formatted = moment(discount.scheduleAt).format("YYYY-MM-DD HH:mm:ss");
+			const formatted = moment.utc(discount.scheduleAt).tz('Asia/Ho_Chi_Minh').format('YYYY-MM-DD HH:mm:ss');
 			const startDate = moment(discount.startDate).format("YYYY-MM-DD");
 			const endDate = moment(discount.endDate).format("YYYY-MM-DD");
 			const channel = await require("../server").channel;
